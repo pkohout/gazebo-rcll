c4c6f9c3dfcf6104b432b913bb68f1bad6ed9e08
Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
2020-11-17 10:44:19 +0100

    mps_comm: MpsDate tracks command registers

--- plugins/src/libs/mps_comm/mps_data.h
+++ plugins/src/libs/mps_comm/mps_data.h
@@ -1,18 +1,42 @@
+/***************************************************************************
+ *  mps_data.h -  OpcUa nodes used to communicate with the mps
+ *
+ *  Created: Thu 16 Nov 2020 13:29:11 CET 13:29
+ *  Copyright  2020 Mostafa Gomaa <gomaa@kbsg.rwth-aachen.de>
+ ****************************************************************************/
 
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#pragma once
 #include "opc_utils.h"
 #include <exception>
 #include <opc/ua/node.h>
 #include <opc/ua/protocol/variant.h>
+
 using namespace OpcUa;
 
 namespace mps_comm {
 
 class MpsData {
+  friend class CommandHandler;
+
   // Registers existing in the MPS, to which it is possible to subscribe to
   enum Registers {
     ACTION_ID = 0,
     BARCODE,
-    DATA,
+    DATA_PAYLOAD1,
+    DATA_PAYLOAD2,
     ERROR,
     SLIDECOUNT,
     STATUS_BUSY,
@@ -25,7 +49,7 @@ class MpsData {
 public:
   MpsData(std::shared_ptr<OpcUa::UaServer> OPCServer,
           std::shared_ptr<spdlog::logger> logger, std::vector<std::string> path)
-      : OPCServer(OPCServer), logger_(logger) {
+      : server_(OPCServer), logger_(logger), base_path_(path) {
 
     // Get parent Object Node descibed by the base path
     Node parent = OPCServer->GetRootNode();
@@ -54,11 +78,11 @@ public:
     reg_nodes_[ERROR] =
         parent.AddVariable(namespaceIdx, "Error", Variant((uint8_t)0));
     // Data Object Node
-    reg_nodes_[DATA] = parent.AddObject(namespaceIdx, "Data");
-    reg_nodes_[DATA].AddVariable(namespaceIdx, "payload1",
-                                 Variant((uint16_t)0));
-    reg_nodes_[DATA].AddVariable(namespaceIdx, "payload2",
-                                 Variant((uint16_t)0));
+    Node n_data = parent.AddObject(namespaceIdx, "Data");
+    reg_nodes_[DATA_PAYLOAD1] =
+        n_data.AddVariable(namespaceIdx, "payload1", Variant((uint16_t)0));
+    reg_nodes_[DATA_PAYLOAD2] =
+        n_data.AddVariable(namespaceIdx, "payload2", Variant((uint16_t)0));
     // Status Object Vars
     Node n_status = parent.AddObject(namespaceIdx, "Status");
     reg_nodes_[STATUS_BUSY] =
@@ -73,10 +97,16 @@ public:
 
   ~MpsData(){};
 
-  std::shared_ptr<OpcUa::UaServer> OPCServer;
-  std::shared_ptr<spdlog::logger> logger_;
+  OpcUa::Node get_node(Registers reg) { return reg_nodes_[reg]; }
 
-  // private:
+  std::vector<Registers> CommandRegisters = {
+      ACTION_ID, DATA_PAYLOAD1, DATA_PAYLOAD2, STATUS_ENABLE, ERROR};
+
+private:
+  std::shared_ptr<OpcUa::UaServer> server_;
+  std::shared_ptr<spdlog::logger> logger_;
+  std::vector<std::string> base_path_;
+
   std::map<Registers, OpcUa::Node> reg_nodes_;
 };
 
